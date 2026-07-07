4e9493e2a0595ae4c3d422d6515fd0325f984d93
codenamelonghorn <65045160+codenamelonghorn@users.noreply.github.com>
2026-07-07 18:18:22 -0500

    Update D_MAIN.C
    FIX*

--- D_MAIN.C
+++ D_MAIN.C
@@ -644,8 +644,8 @@ void D_DoAdvanceDemo (void)
     {
       case 0:
         gamestate = GS_DEMOSCREEN;
-            
-            pagetic = TICRATE * 11;
+        if ( gamemode == commercial )
+            pagetic = TICRATE * 11; // Changed by Tails: 9-14-99
         else
             pagetic = 200;
         pagename = "TITLEPIC";
