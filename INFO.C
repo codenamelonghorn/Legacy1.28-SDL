35c70e470c25b8718e651bb519137a1b1396b04c
codenamelonghorn <65045160+codenamelonghorn@users.noreply.github.com>
2026-07-12 22:52:28 -0500

    Update INFO.C
    Originally referenced code from 1.30

--- INFO.C
+++ INFO.C
@@ -10,7 +10,7 @@
 
 #include "p_mobj.h"
 
-char *sprnames[NUMSPRITES+1] = {
+char *sprnames[NUMSPRITES] = {
     "TROO","SHTG","PUNG","PISG","PISF","SHTF","SHT2","CHGG","CHGF","MISG",
     "MISF","SAWG","PLSG","PLSF","BFGG","BFGF","BLUD","PUFF","BAL1","BAL2",
     "PLSS","PLSE","MISL","BFS1","BFE1","BFE2","TFOG","IFOG","PLAY","POSS",
@@ -24,8 +24,7 @@ char *sprnames[NUMSPRITES+1] = {
     "POL3","POL1","POL6","GOR2","GOR3","GOR4","GOR5","SMIT","COL1","COL2",
     "COL3","COL4","CAND","CBRA","COL6","TRE1","TRE2","ELEC","CEYE","FSKU",
     "COL5","TBLU","TGRN","TRED","SMBT","SMGT","SMRT","HDB1","HDB2","HDB3",
-    "HDB4","HDB5","HDB6","POB1","POB2","BRS1","TLMP","TLP2","SMOK","SPLA",
-    NULL /* shit! 19990907 by Kin */
+    "HDB4","HDB5","HDB6","POB1","POB2","BRS1","TLMP","TLP2","SMOK","SPLA"
 };
 
 
